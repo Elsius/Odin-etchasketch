e3de2ea13249598eb2d602adad668a138a712b6b
Elsius <sourly95@gmail.com>
2022-07-24 20:23:48 -0700

    Custom grid size enabled
    cleaning up some code

--- js.js
+++ js.js
@@ -1,18 +1,20 @@
 const newGameButton = document.querySelector('#newGame');
-let etch = null;
 newGameButton.addEventListener('click', createGame)
 
 //create game area 16x16 grid
 //this method seperates the grid in columns, can we do it in 1 box?
 function createGame() {
-    //newGameButton.style.display = 'none';
     const gridBox = document.querySelector('#gridBox');
+    let gridX = document.getElementById('gridX').value,
+    gridY = document.getElementById('gridY').value;
+    if (gridX > 100) gridX = 100
+    if (gridY > 100) gridY = 100
     gridBox.innerHTML = '';
-    for (i = 0; i < 4; i++) { //loop creates a column to put items in
+    for (i = 0; i < gridX; i++) { //loop creates a column to put items in
         const column = document.createElement('div')
         column.setAttribute('class',`column${i}`)
         gridBox.appendChild(column)
-        for (j = 0; j < 4; j++) { //loop for each item in the row
+        for (j = 0; j < gridY; j++) { //loop for each item in the row
             const divBox = document.createElement('div')
             divBox.setAttribute("id", `${i},${j}`)
             divBox.setAttribute('class','divGridBox')
